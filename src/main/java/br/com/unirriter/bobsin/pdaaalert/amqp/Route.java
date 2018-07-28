6dad9e400a32228b09158c6a9c7bcc834bf0d315
Gian Regis Frick <grfrick@stefanini.com>
2018-07-28 10:17:11 -0300

    colocando properties

--- src/main/java/br/com/unirriter/bobsin/pdaaalert/amqp/Route.java
+++ src/main/java/br/com/unirriter/bobsin/pdaaalert/amqp/Route.java
@@ -3,19 +3,28 @@ package br.com.unirriter.bobsin.pdaaalert.amqp;
 import org.apache.camel.Exchange;
 import org.apache.camel.LoggingLevel;
 import org.apache.camel.spring.SpringRouteBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Route extends SpringRouteBuilder {
 
+	private static final String AMQP_QUEUE = "amqp:queue";
+	private static final String QUEUE_TEST = "test";
+	private static final String QUEUE_PROD = "a-monitor-alerts";
+
+	@Value("${queue.prod}")
+	private boolean isProd;
+			
     @Override
     public void configure() {
-
+    	String queue = isProd ? QUEUE_PROD : QUEUE_TEST;
+    	
         onException(Exception.class)
                 .handled(false)
                 .log(LoggingLevel.ERROR, "Error in processing route!");
 
-        from("amqp:queue:test")
+        from(AMQP_QUEUE + queue)
                 .setHeader(Exchange.CONTENT_TYPE, constant("application/octet-stream"))
                 .setHeader(Exchange.HTTP_METHOD, constant(org.apache.camel.component.http4.HttpMethods.POST))
                 .log("Content ${body}")
--- src/main/java/br/com/unirriter/bobsin/pdaaalert/config/QueueConfig.java
+++ src/main/java/br/com/unirriter/bobsin/pdaaalert/config/QueueConfig.java
@@ -1,6 +1,7 @@
 package br.com.unirriter.bobsin.pdaaalert.config;
 
 import org.apache.camel.component.amqp.AMQPConnectionDetails;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 /*
@@ -15,10 +16,43 @@ public class QueueConfig {
     private static final String AMQP_SERVICE_PORT = "5671";
     private static final String AMQP_SERVICE_USERNAME = System.getenv("user");
     private static final String AMQP_SERVICE_PASSWORD = System.getenv("pass");
+    
+    @Value("${ampq.service.username}")
+    private String userAmq;
 
+    @Value("${ampq.service.password}")
+    private String passwAmq;
+    
     public AMQPConnectionDetails securedAmqpConnection() {
-        return new AMQPConnectionDetails("amqps://"+AMQP_SERVICE_HOST+":"+AMQP_SERVICE_PORT,
-                AMQP_SERVICE_USERNAME, AMQP_SERVICE_PASSWORD);
+    	if (null != AMQP_SERVICE_USERNAME) {
+    		System.out.println(">> user from HEROKU");
+    		setUserAmq(AMQP_SERVICE_USERNAME);
+    	}
+    	
+    	if (null != AMQP_SERVICE_PASSWORD) {
+    		System.out.println(">> pass from HEROKU");
+    		setPasswAmq(AMQP_SERVICE_PASSWORD);
+    	}
+    	
+        return new AMQPConnectionDetails("amqps://" + AMQP_SERVICE_HOST+":" + AMQP_SERVICE_PORT, 
+        											  getUserAmq(), 
+        											  getPasswAmq());
     }
+
+	private String getUserAmq() {
+		return userAmq;
+	}
+
+	private void setUserAmq(String userAmq) {
+		this.userAmq = userAmq;
+	}
+
+	private String getPasswAmq() {
+		return passwAmq;
+	}
+
+	private void setPasswAmq(String passwAmq) {
+		this.passwAmq = passwAmq;
+	}
 
 }
